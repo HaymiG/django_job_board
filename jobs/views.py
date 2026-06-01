46f6807fa56c394f6cd4aabdd358137ee28554f5
haymig <haymig75@gmail.com>
2026-06-01 20:32:10 +0300

    updated the job app view

--- jobs/views.py
+++ jobs/views.py
@@ -1,1 +1,16 @@
 from django.shortcuts import render
+
+from .models import Application, Company, Job
+
+
+def home(request):
+    recent_jobs = Job.objects.filter(is_active=True).select_related("company")[:6]
+    context = {
+        "recent_jobs": recent_jobs,
+        "stats": {
+            "jobs": Job.objects.filter(is_active=True).count(),
+            "companies": Company.objects.count(),
+            "applications": Application.objects.count(),
+        },
+    }
+    return render(request, "jobs/home.html", context)
