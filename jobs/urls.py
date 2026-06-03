5598d2340a07c3ec9369dce1984863017997a9ff
haymig <haymig75@gmail.com>
2026-06-03 10:01:04 +0300

    extending the job app url

--- jobs/urls.py
+++ jobs/urls.py
@@ -6,4 +6,9 @@ app_name = "jobs"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("browse/", views.job_list, name="job_list"),
+    path("job/<int:pk>/", views.job_detail, name="job_detail"),
+    path("job/create/", views.create_job, name="create_job"),
+    path("job/<int:pk>/edit/", views.edit_job, name="edit_job"),
+    path("job/<int:pk>/delete/", views.delete_job, name="delete_job"),
 ]
